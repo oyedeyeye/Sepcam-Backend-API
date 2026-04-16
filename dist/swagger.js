8c184da4006ad2c8be71acbaa90984317ac066dd
Hezekiah Oyemade <oyedey@gmail.com>
2026-04-16 12:48:20 +0100

    Decumentation with Swagger

--- dist/swagger.js
+++ dist/swagger.js
@@ -4,13 +4,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const swagger_autogen_1 = __importDefault(require("swagger-autogen"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const doc = {
     info: {
         title: 'Sepcam API v2',
         description: 'API documentation for Sepcam V2',
     },
-    host: 'localhost:5000', // Update this based on the actual host in development or production
-    schemes: ['http', 'https'],
+    // In OpenAPI 3, 'host' and 'schemes' are replaced by 'servers'.
+    servers: [
+        {
+            url: process.env.API_BASE_URL || `http://localhost:${process.env.PORT || 5000}`,
+            description: 'Current Environment or Production Server'
+        },
+        // {
+        //   url: 'https://api.sepcam.com', // Replace with your actual production domain
+        //   description: 'Production Server'
+        // }
+    ],
 };
 const outputFile = './swagger-output.json'; // Outputs to src/swagger-output.json (relative to where it runs or we should put it inside src)
 const endpointsFiles = ['./app.ts'];
